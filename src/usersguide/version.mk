1b1bb31439e01621e62bf773f82f028468008239
Mason Katz <mason.katz@gmail.com>
2008-07-21 21:23:28 +0000

    docs good

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -5,6 +5,6 @@ SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
 SUMMARY_ARCHITECTURE    = i386, x86_64
 
-ROLL_REQUIRES           = base hpc kernel os1 os2 $(ROLL)
-ROLL_CONFLICTS          =
+ROLL_REQUIRES           = base hpc kernel java os1 os2 os3 os4 os5 service-pack $(ROLL)
+ROLL_CONFLICTS          = xen
 
