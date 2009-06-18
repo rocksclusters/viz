68d8c814d1094aaa12c39ea31996763fa17c2f6d
Mason Katz <mason.katz@gmail.com>
2009-06-18 18:08:03 +0000

    - setup nvidia driver on frontend - fix roll deps in usersguide

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -3,8 +3,8 @@ RELEASE = 0
 
 SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
-SUMMARY_ARCHITECTURE    = i386, x86_64
+SUMMARY_ARCHITECTURE    = x86_64
 
-ROLL_REQUIRES           = base hpc kernel java os1 os2 os3 os4 os5 service-pack $(ROLL)
+ROLL_REQUIRES           = base hpc kernel java os1 os2 $(ROLL)
 ROLL_CONFLICTS          = xen
 
