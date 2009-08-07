364cb3976de2671ebd2f6e6680b566995c849920
Mason Katz <mason.katz@gmail.com>
2009-08-07 20:27:41 +0000

    *** empty log message ***

--- src/nvidia/version.mk
+++ src/nvidia/version.mk
@@ -1,6 +1,6 @@
 NAME = nvidia-driver
 RELEASE = 1
 
-NVIDIA_DRIVER = NVIDIA-Linux-x86_64-180.60-pkg2.run
+NVIDIA_DRIVER = NVIDIA-Linux-x86_64-185.18.29-pkg2.run
 CUDA_DRIVER = cudadriver_2.2_linux_64_185.18.08-beta.run
 
