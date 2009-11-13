9fa77662ec256b38446d67b7df8f39e38f42c613
Mason Katz <mason.katz@gmail.com>
2009-11-13 20:19:36 +0000

    new drivers

--- src/nvidia/version.mk
+++ src/nvidia/version.mk
@@ -1,6 +1,5 @@
 NAME = nvidia-driver
 RELEASE = 1
 
-NVIDIA_DRIVER = NVIDIA-Linux-x86_64-185.18.29-pkg2.run
-CUDA_DRIVER = cudadriver_2.2_linux_64_185.18.08-beta.run
+NVIDIA_DRIVER = NVIDIA-Linux-x86_64-190.42-pkg2.run
 
