2e8d2b2e23155be224868e0825b242019df7e923
Mason Katz <mason.katz@gmail.com>
2011-01-28 02:17:28 +0000

    Docbook cleanup (using Viz Roll as proto-type) - consistent entity naming (no more mixing of '-' and '_') - roll compat page only lists specified rolls (version.mk) - added note about using all OS cds with non-core rolls (e.g. viz) - added entities for roll names, and bools - logical styles used instead of direct formatting   e.g. constant vs. emphasis Works for Viz (needs new devel env installed) TODO: Update Base Roll to further standardize (slow) TODO: Cleanup all other Rolls (fast)

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,11 +1,12 @@
 NAME    = roll-$(ROLL)-usersguide
-RELEASE = 1
+RELEASE = 2
 RPM.ARCH = noarch
 
 SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
 SUMMARY_ARCHITECTURE    = x86_64
 
-ROLL_REQUIRES           = base hpc kernel os1 os2 $(ROLL)
+ROLL_REQUIRES           = base kernel service-pack os
+ROLL_REQUIRES_FULL_OS	= 1
 ROLL_CONFLICTS          = xen
 
